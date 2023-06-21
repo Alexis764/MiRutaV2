60e5351ea5e4860aea70143008600abb525e047b
RaulP27 <rperalta0327@gmail.com>
2023-06-21 15:28:04 -0500

    Cambios al Switch y Diseño de Home

--- app/src/main/java/com/example/mirutav2/home/HomeActivity.kt
+++ app/src/main/java/com/example/mirutav2/home/HomeActivity.kt
@@ -27,8 +27,7 @@ class HomeActivity : AppCompatActivity() {
     //Variables para componentes
     private lateinit var nrvHome: NavigationRailView
     private lateinit var fcvHome: NavHostFragment
-    //Variable Switch(Tema)
-    private lateinit var switch: Switch
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +35,6 @@ class HomeActivity : AppCompatActivity() {
 
         initComponent()
         initController()
-        initSwitch()
 
     }
 
@@ -45,10 +43,6 @@ class HomeActivity : AppCompatActivity() {
     private fun initComponent() {
         nrvHome = findViewById(R.id.nrvHome)
         fcvHome = supportFragmentManager.findFragmentById(R.id.fcvHome) as NavHostFragment
-
-        //Swith
-        switch = findViewById(R.id.switch1)
-
     }
 
 
@@ -57,30 +51,5 @@ class HomeActivity : AppCompatActivity() {
         navController = fcvHome.navController
         nrvHome.setupWithNavController(navController)
     }
-
-    private fun startMain() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
-
-
-    // Switch para Tema Claro/Oscuro
-    private fun initSwitch() {
-
-        val sp = getSharedPreferences("SP", Context.MODE_PRIVATE)
-        val editor = sp.edit()
-
-        switch.setOnClickListener {
-
-            if (switch.isChecked) {
-                editor.putInt("Theme",0)
-            } else {
-                editor.putInt("Theme",1)
-            }
-            editor.commit()
-
-        }
-    }
-
 
 }
--- app/src/main/java/com/example/mirutav2/home/HomeFragment.kt
+++ app/src/main/java/com/example/mirutav2/home/HomeFragment.kt
@@ -1,60 +1,58 @@
 package com.example.mirutav2.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.example.mirutav2.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var switch: Switch
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView: View = inflater.inflate(R.layout.fragment_home, container, false)
+        initComponent(rootView)
+        initSwitch()
+        return rootView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun initComponent(rootView: View) {
+        switch = rootView.findViewById(R.id.switch1)
+    }
+
+    private fun initSwitch() {
+        val activity = requireActivity()
+        val sp: SharedPreferences = activity.getSharedPreferences("SP", Context.MODE_PRIVATE)
+        val editor = sp.edit()
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                editor.putInt("Theme", 0)
+            } else {
+                editor.putInt("Theme", 1)
             }
+            editor.apply()
+            setDayNight(activity as AppCompatActivity)
+        }
+    }
+
+    private fun setDayNight(activity: AppCompatActivity) {
+        val sp: SharedPreferences = activity.getSharedPreferences("SP", Context.MODE_PRIVATE)
+        val theme = sp.getInt("Theme", 1)
+        if (theme == 0) {
+            activity.delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+        } else {
+            activity.delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+        }
     }
 }
