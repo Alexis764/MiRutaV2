39e228747f442306723b02093b7ca003aaebaff8
Alexis Arevalo <arevaloalexis764@gmail.com>
2023-07-13 11:31:43 -0500

    Importacion de resources para poder ejecutar

--- app/src/main/java/com/example/mirutav2/home/admin/driver/UpdateDriverActivity.kt
+++ app/src/main/java/com/example/mirutav2/home/admin/driver/UpdateDriverActivity.kt
@@ -2,6 +2,7 @@ package com.example.mirutav2.home.admin.driver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mirutav2.R
 
 class UpdateDriverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
