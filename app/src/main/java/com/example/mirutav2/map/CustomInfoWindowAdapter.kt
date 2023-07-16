88caa16fe796bcc4bdb2095fe96530646b0ede58
Alexis Arevalo <arevaloalexis764@gmail.com>
2023-07-16 02:08:06 -0500

    Actualizacion de la ubicacion de los buses en el mapa y cambio en los diseños del mapa

---
+++ app/src/main/java/com/example/mirutav2/map/CustomInfoWindowAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.mirutav2.map
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.mirutav2.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+
+class CustomInfoWindowAdapter(private val context: Context): GoogleMap.InfoWindowAdapter {
+
+    @SuppressLint("InflateParams")
+    override fun getInfoContents(marker: Marker): View? {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val rootView = inflater.inflate(R.layout.item_custom_window, null)
+
+        val tvCustomStopName = rootView.findViewById<TextView>(R.id.tvCustomStopName)
+        val ivCustomStopImage = rootView.findViewById<ImageView>(R.id.ivCustomStopImage)
+
+        tvCustomStopName.text = marker.title
+        try {
+            Glide.with(context)
+                .load(marker.tag)
+                .into(ivCustomStopImage)
+
+        } catch (e: Exception) {
+            Log.e("StopImageCustom", "No carga la imagen")
+        }
+
+        return rootView
+    }
+
+    override fun getInfoWindow(marker: Marker): View? {
+        return null
+    }
+
+}
